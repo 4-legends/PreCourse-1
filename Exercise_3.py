e7a5cc93e6aa9ea19b784fb7e436893aa7b1590f
unitx <paresh@unitxlabs.com>
2024-06-14 19:44:34 -0700

    chore: Exercise 3

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,13 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+    
+    def __repr__(self):
+        print("Current Data: ", self.data)
+        print("Next Data: ", self.next.data)
+        return ""
     
 class SinglyLinkedList:
     def __init__(self):
@@ -10,13 +17,19 @@ class SinglyLinkedList:
         Create a new singly-linked list.
         Takes O(1) time.
         """
-        self.head = None
+        self.head = ListNode()
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head.data is None:
+            self.head.data = data
+        else:
+            new_node = ListNode(data)
+            new_node.next = self.head
+            self.head = new_node
         
     def find(self, key):
         """
@@ -24,9 +37,48 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        temp = self.head
+        while temp is not None:
+            if temp.data == key:
+                return temp
+            temp = temp.next
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        temp = self.head
+        prev = None
+        while temp is not None:
+            if temp.data == key:
+                if prev is not None:
+                    prev.next = temp.next
+                else:
+                    self.head = temp.next
+                return
+            prev = temp
+            temp = temp.next
+        return None
+
+    def show(self):
+        """
+        Shows the elements of the list
+        """
+        stack = []
+        temp = self.head
+        while temp is not None:
+            stack.append(temp.data)
+            temp = temp.next
+        return stack
+    
+a = SinglyLinkedList()
+a.append(1)
+a.append(2)
+a.append(3)
+print(a.show())
+a.remove(2)
+a.append(4)
+print(a.show())
+print(a.find(3))
